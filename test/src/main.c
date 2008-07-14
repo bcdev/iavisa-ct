367229fe83844b3ba82ddfa9e7105a8c03187d7d
ralf <ralf@fc2be322-dc32-0410-aa07-d0f95ae639be>
2008-07-14 07:21:43 +0000

    changed test routine
    git-svn-id: https://www.brockmann-consult.de/svn/cs/iavisact-1.x/trunk@631 fc2be322-dc32-0410-aa07-d0f95ae639be

--- test/src/main.c
+++ test/src/main.c
@@ -1,4 +1,6 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 
 #include "gt.h"
 
@@ -6,26 +8,56 @@ int main(int argc, char* argv[]) {
 	char* initId = "";
 	if (argc > 0) {
 		initId = argv[1];
-	} 
+	}
 
-	gtInit(TRAINING, TEST_B, initId, 0);
-	printf("TEST B: %d\n", gtSampleCount());
+	gtInit(TRAINING, TEST_E, initId, "test.par");
+	const int32_t sampleCount = gtSampleCount();
 
-	gtInit(TRAINING, TEST_D, initId, 0);
-	printf("TEST D: %d\n", gtSampleCount());
+	int32_t* truths = (int32_t*) malloc(sampleCount * sizeof(int32_t));
+	gtGetCloudTruth(truths);
 
-	gtInit(TRAINING, TEST_E, initId, 0);
-	printf("TEST E: %d\n", gtSampleCount());
+	bool* estimates = (bool*) malloc(sampleCount * sizeof(bool));
 	
-	gtInit(TRAINING, TEST_F, initId, 0);
-	printf("TEST F: %d\n", gtSampleCount());
+	const clock_t start = clock();
+	gtGetCloudGuess(estimates);
+	const clock_t stop = clock();
+	
+	int cloudCloud = 0;
+	int cloudClear = 0;
+	int clearCloud = 0;
+	int clearClear = 0;
 
-	gtInit(TRAINING, TEST_G, initId, 0);
-	printf("TEST G: %d\n", gtSampleCount());
+	// build contingency table
+	int i;
+	for (i = 0; i < sampleCount; ++i) {
+		if (estimates[i]) {
+			if (truths[i] != 1) {
+				++cloudCloud;
+			} else {
+				++cloudClear;
+			}
+		} else {
+			if (truths[i] != 1) {
+				++clearCloud;
+			} else {
+				++clearClear;
+			}
+		}
+	}
 
-	gtInit(TRAINING, TEST_H, initId, 0);
-	printf("TEST H: %d\n", gtSampleCount());
-	gtExit();
+	printf("cloud-cloud: %d\n", cloudCloud);
+	printf("cloud-clear: %d\n", cloudClear);
+	printf("clear-cloud: %d\n", clearCloud);
+	printf("clear-clear: %d\n", clearClear);
+	printf("\n");
+	printf("test execution time: %d microseconds\n", (stop - start) / 1000);
+
+	if (estimates != 0) {
+		free(estimates);
+	}
+	if (truths != 0) {
+		free(truths);
+	}
 
 	return 0;
 }
